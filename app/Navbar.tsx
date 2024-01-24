006f143c874fc41d8b07c3fce2479568b0cd8968
Behzad Aminipour <behzadaminipour87@gmail.com>
2024-01-24 13:05:04 +0330

    active link navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,28 +1,38 @@
+"use client";
 import { AiFillBug } from "react-icons/ai";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
 const Navbar = () => {
+  const currentPath = usePathname();
+
   const links = [
     {
-      label: "داشبورد",
+      label: "Dashboard",
       href: "/",
     },
     {
-      label: "موضوع",
+      label: "Issues",
       href: "/issues",
     },
   ];
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/" className="mx-5">
+      <Link href="/">
         <AiFillBug size={24} />
       </Link>
-      <ul className="flex gap-6">
+      <ul className="flex space-x-6">
         {links.map((link) => (
           <Link
             href={link.href}
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import Navbar from "./Navbar";
 
-const vazir = localFont({
-  src: "../public/fonts/Vazirmatn-Regular.woff2",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" dir="rtl">
-      <body className={vazir.className}>
+    <html lang="en">
+      <body className={inter.className}>
         <Navbar />
         {children}
       </body>
