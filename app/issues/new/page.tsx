2e11fb7417209196870ceb93d590b940f6afa32a
Behzad Aminipour <behzadaminipour87@gmail.com>
2024-01-24 19:32:56 +0330

    Customize Radix UI theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -8,7 +8,7 @@ const NewIssuePage = () => {
         <TextField.Input placeholder="Title" />
       </TextField.Root>
       <TextArea placeholder="Description..."/>
-      <Button>Submit new issue</Button>
+      <Button>Submit New Issue</Button>
     </div>
   );
 };
