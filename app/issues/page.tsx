2bf85212d97b999e4b92d436d9072c326a2e64d8
Behzad Aminipour <behzadaminipour87@gmail.com>
2024-01-27 18:15:58 +0330

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,5 @@
 import { Table } from "@radix-ui/themes";
-import Link from "next/link";
+import Link from "../components/Link";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
