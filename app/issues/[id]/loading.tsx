405b7c94ce0f290bbc09d69e42932261f50e960e
Behzad Aminipour <behzadaminipour87@gmail.com>
2024-01-27 21:59:21 +0330

    Refactor:Organize import statement

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,20 +1,19 @@
-import { Flex, Card, Box } from "@radix-ui/themes"
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
+import { Box, Card, Flex } from "@radix-ui/themes";
 
 const LoadingDetailIssuePage = () => {
   return (
     <Box className="max-w-xl">
-    <Skeleton/>
-    <Flex gap="2" my="2">
-      <Skeleton width='5rem'/>
-      <Skeleton width='8rem'/>
-    </Flex>
-    <Card className="prose" mt="4">
-    <Skeleton count={3}/>
-    </Card>
-  </Box>
-  )
-}
+      <Skeleton />
+      <Flex gap="2" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
+};
 
-export default LoadingDetailIssuePage
+export default LoadingDetailIssuePage;
